20e9d53be9b6bb9037c7a81fdce920269468562c
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-06-20 20:40:25 +0300

    implement fetching headlines in HeadlinesPage component

--- src/app/pages/HeadlinesPage/index.tsx
+++ src/app/pages/HeadlinesPage/index.tsx
@@ -6,13 +6,34 @@
 import * as React from 'react';
 import styled from 'styled-components/macro';
 
+import { useHeadlinesSlice } from './slice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { selectHeadlines } from './slice/selectors';
+import { useEffect } from 'react';
+
 interface Props {}
 
 export function HeadlinesPage(props: Props) {
+  const { actions } = useHeadlinesSlice();
+  const dispatch = useDispatch();
+  const { headlines } = useSelector(selectHeadlines);
+
+  useEffect(() => {
+    dispatch(actions.sagaGetHeadlines());
+  }, [dispatch, actions]);
+
   return (
-    <Div>
+    <>
       <span>Headlines</span>
-    </Div>
+      <Div>
+        {headlines?.map((headline, i) => (
+          <div key={`headline-${i}`}>
+            <img src={headline.imageUrl} alt={`headline-${i}`} />
+          </div>
+        ))}
+      </Div>
+    </>
   );
 }
 
