b34954383a44a1b017fdd3560503285416326711
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-06-20 12:59:59 +0300

    /headlines setup

---
+++ src/app/pages/HeadlinesPage/index.tsx
@@ -0,0 +1,19 @@
+/**
+ *
+ * HeadlinesPage
+ *
+ */
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+interface Props {}
+
+export function HeadlinesPage(props: Props) {
+  return (
+    <Div>
+      <span>Headlines</span>
+    </Div>
+  );
+}
+
+const Div = styled.div``;
