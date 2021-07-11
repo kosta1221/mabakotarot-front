101166395a8a91b86053aea79a595df8746e9fcf
michefraim <77235028+michefraim@users.noreply.github.com>
2021-07-11 16:58:55 +0300

    Added tooltip for the unique headlines switch

--- src/app/components/Appbar/UniqueOrNotSwitch/index.tsx
+++ src/app/components/Appbar/UniqueOrNotSwitch/index.tsx
@@ -7,12 +7,24 @@ import * as React from 'react';
 import styled from 'styled-components/macro';
 
 import Switch from '@material-ui/core/Switch';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { appbarActions } from '../slice';
 import { selectAppbar } from '../slice/selectors';
+import { withStyles } from '@material-ui/core/styles';
 
 interface Props {}
+
+const UniqueOrNotSwitchTooltip = withStyles(theme => ({
+  tooltip: {
+    backgroundColor: '#f5f5f9',
+    color: 'black',
+    maxWidth: 200,
+    fontSize: theme.typography.pxToRem(15),
+    border: '1px solid #dadde9',
+  },
+}))(Tooltip);
 
 export function UniqueOrNotSwitch(props: Props) {
   const dispatch = useDispatch();
@@ -23,12 +35,14 @@ export function UniqueOrNotSwitch(props: Props) {
   };
 
   return (
-    <StyledSwitch
-      checked={showUniqueOnly}
-      onChange={handleUniquenessSwitchChange}
-      name="UniqueOrNotSwitch"
-      inputProps={{ 'aria-label': 'unique or not toggle switch' }}
-    />
+    <UniqueOrNotSwitchTooltip title="הצג רק כותרות עם שינויים">
+      <StyledSwitch
+        checked={showUniqueOnly}
+        onChange={handleUniquenessSwitchChange}
+        name="UniqueOrNotSwitch"
+        inputProps={{ 'aria-label': 'unique or not toggle switch' }}
+      />
+    </UniqueOrNotSwitchTooltip>
   );
 }
 
